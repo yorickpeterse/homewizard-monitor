8fc85ff06c42cf0f5d781bfac2ada5bac90aea8e
Yorick Peterse <git@yorickpeterse.com>
2023-02-21 22:47:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# HomeWizard Monitor
+
+A simple [Fish](https://fishshell.com/) for monitoring the output of a
+[HomeWizard P1 meter](https://www.homewizard.com/), and sending it to an
+InfluxDB/VictoriaMetrics database.
+
+# License
+
+All source code in this repository is licensed under the Mozilla Public License
+version 2.0, unless stated otherwise. A copy of this license can be found in the
+file "LICENSE".
---
+++ monitor.fish
@@ -0,0 +1,39 @@
+#!/usr/bin/env fish
+
+if ! test -n "$P1_IP"
+    echo 'The P1_IP variable must be set'
+    exit 1
+end
+
+if ! test -n "$DB_IP"
+    echo 'The DB_IP variable must be set'
+    exit 1
+end
+
+if ! test -n "$DB_PORT"
+    echo 'The DB_PORT variable must be set'
+    exit 1
+end
+
+while true
+    set data (
+        curl --fail \
+            --show-error \
+            --silent \
+            --connect-timeout 5 \
+            --max-time 5 \
+            "http://$P1_IP/api/v1/data"
+    )
+
+    set phase1 (echo $data | jq '."active_power_l1_w"')
+    set phase2 (echo $data | jq '."active_power_l2_w"')
+    set phase3 (echo $data | jq '."active_power_l3_w"')
+
+    # The API reports the data in Kilowatt/hour, but we want a base unit so we
+    # convert it to watts/hour.
+    set consumed (echo $data | jq '."total_power_import_kwh" * 1000')
+    set produced (echo $data | jq '."total_power_export_kwh" * 1000')
+
+    echo "electricity phase1=$phase1,phase2=$phase2,phase3=$phase3,produced=$produced,consumed=$consumed" | nc -u 127.0.0.1 8089
+    sleep 60
+end
